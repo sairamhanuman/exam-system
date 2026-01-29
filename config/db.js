33d10533d035f1fae6acae9bdbc0f03b6479d987
Ramu Sairam <sairamhanuman85@gmail.com>
2026-01-29 22:34:19 +0530

    fix mysql railway connection

--- config/db.js
+++ config/db.js
@@ -1,17 +1,32 @@
 const mysql = require("mysql2");
 
+console.log("====== MYSQL DEBUG ======");
+console.log("HOST:", process.env.MYSQL_HOST);
+console.log("USER:", process.env.MYSQL_USER);
+console.log("DB:", process.env.MYSQL_DATABASE);
+console.log("PORT:", process.env.MYSQL_PORT);
+console.log("PASSWORD EXISTS:", !!process.env.MYSQL_PASSWORD);
+console.log("=========================");
+
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
-
-  ssl: {
-    rejectUnauthorized: false
-  },
-
+  host: process.env.MYSQL_HOST,          // mysql.railway.internal
+  user: process.env.MYSQL_USER,          // root
+  password: process.env.MYSQL_PASSWORD,  // auto generated
+  database: process.env.MYSQL_DATABASE,  // railway
+  port: process.env.MYSQL_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("❌ MYSQL CONNECTION ERROR:");
+    console.error(err);
+  } else {
+    console.log("✅ MYSQL CONNECTED SUCCESSFULLY");
+    connection.release();
+  }
+});
+
+module.exports = pool;
