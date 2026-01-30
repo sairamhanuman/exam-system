6dfcb05b35f54ed25890e7933db795e65a6fcd7e
Ramu Kavalluri <sairamhanuman85@gmail.com>
2026-01-30 10:44:01 +0530

    Update staff.js

--- routes/staff.js
+++ routes/staff.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
 
 /* ================= PHOTO STORAGE ================= */
 
