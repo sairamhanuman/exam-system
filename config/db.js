f75e495121d98d0a0175b05b0eb81f41dd6adb07
Ramu Kavalluri <sairamhanuman85@gmail.com>
2026-01-30 07:10:41 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
-const mysql = require("mysql2");
+  const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   ssl: false
 });
 
